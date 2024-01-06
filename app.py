179b2c5c487861bc4291c865a3014b6b944cae48
chaplin96 <3ma510.96ki@gmail.com>
2024-01-06 17:04:10 +0900

    PWA化した大喜利ドリル

---
+++ app.py
@@ -0,0 +1,31 @@
+import re
+import os
+from flask import Flask, render_template, send_file
+from datetime import datetime, timedelta, timezone
+import random
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def oogiri_drill():
+    # TODO 大喜利回答例、スコア高いのを表示した方が参考になるよな
+    with open("data/oogiri.json", "r", encoding="utf-8") as f:
+        tmp_oogiri_list = json.load(f)
+    
+    oogiri = random.choice(tmp_oogiri_list)
+    oogiri["answer_samples"] = random.sample(oogiri["answer_samples"], 3)
+
+    return render_template("drill.html", oogiri=oogiri)
+
+@app.route('/sw.js')
+def serve_sw():
+    return send_file('sw.js', mimetype='application/javascript')
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_file('manifest.json', mimetype='application/manifest+json')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+    # app.run(debug=True, host="0.0.0.0", port=80)
